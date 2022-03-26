39afc8a2200a4bc4f6ab66aaf9c3dd8b060aed76
ShomaA0321 <shomaa2003@gmail.com>
2022-03-26 21:42:40 +0900

    信号を生成

--- stftPracticeMain.m
+++ stftPracticeMain.m
@@ -2,6 +2,24 @@
 clear; close all; clc;
 
 % 440Hz＋880Hzの信号（10秒，サンプリング周波数16kHz）を生成
+second = 10;
+sampling_freq = 16000;
+sampling_inter = 1 / sampling_freq;
+sample_count = sampling_freq * second;
+
+t_vec = linspace(sampling_inter, second, sample_count);
+sample_vec = zeros(sample_count, 1)';
+
+freq_base = 440;
+amp = 0.4;
+for i = 1:2
+    omega = 2 * pi * i * freq_base;
+    sample_vec = sample_vec + amp * sin(omega * t_vec);
+end
+
+%チェック
+%audiowrite("test.wav", sample_vec, sampling_freq);
+
 
 % STFT実装
 
